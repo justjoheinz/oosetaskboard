f60d562ef7d95d497a1756fb822bf7e809b0859e
Marcus Nowotny <marcus@interactive-matter.eu>
2012-03-23 14:39:15 +0100

    it is much nicer with a proper logging configuration istead of stdout

--- test/src/de/oose/taskboard/tests/TaskBoardSeleniumTest.java
+++ test/src/de/oose/taskboard/tests/TaskBoardSeleniumTest.java
@@ -1,22 +1,29 @@
+package de.oose.taskboard.tests;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TaskBoardSeleniumTest {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(TaskBoardSeleniumTest.class);
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		//generate a simple driver
+		LOGGER.info("Creating Firefox Driver");
 		WebDriver driver = new FirefoxDriver();
-		System.out.println("Getting home page");
+		LOGGER.info("Getting home page");
 		//get the main page of the task board
 		driver.get("http://localhost:8080/taskboard/");
-		System.out.println("verifying that we got the home page");
+		LOGGER.debug("verifying that we got the home page");
 		assertThat(driver.getTitle(),is("Taskboard"));
 
 	}
