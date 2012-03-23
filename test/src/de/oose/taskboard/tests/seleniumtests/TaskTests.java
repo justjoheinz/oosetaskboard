5a721e60e6c183ad2249ea7b3d955586674385b8
Marcus Nowotny <marcus@interactive-matter.eu>
2012-03-23 15:52:43 +0100

    somehow like this we could create a new task (does not work yet)

--- test/src/de/oose/taskboard/tests/seleniumtests/TaskTests.java
+++ test/src/de/oose/taskboard/tests/seleniumtests/TaskTests.java
@@ -3,10 +3,13 @@ package de.oose.taskboard.tests.seleniumtests;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.notNullValue;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,9 +25,32 @@ public class TaskTests implements SeleniumTest {
 
 	private void createTask(WebDriver driver) {
 		LOGGER.info("Creating a new Task");
+		//Search the new task button and click it
 		WebElement newTaskButton = driver.findElement(By.cssSelector("button.gwt-Button"));
-		assertThat(newTaskButton,not(is((WebElement)null)));
+		assertThat(newTaskButton,notNullValue());
 		newTaskButton.click();
+		//wait for the pop up to open - we simply wait until we see a gwt text area
+		(new WebDriverWait(driver, SeleniumTest.GWT_JS_TIMEOUT)).until(new ExpectedCondition<Boolean>(){
+			public Boolean apply(WebDriver d) {
+				return d.findElement(By.className("gwt-TextArea"))!=null;
+			}
+		});
+		
+		WebElement titleInput = driver.findElement(By.cssSelector("input.gwt-TextBox"));
+		assertThat(titleInput,notNullValue());
+		LOGGER.debug("Setting the title");
+		titleInput.sendKeys("This is the official test taks");
+		
+		WebElement descriptionInput = driver.findElement(By.cssSelector("textarea.gwt-TextArea"));
+		assertThat(descriptionInput,notNullValue());
+		LOGGER.debug("Setting the description");
+		descriptionInput.sendKeys("let's see how selenium handles this!");
+		
+		WebElement saveButton = driver.findElement(By.cssSelector("button.gwt-Button"));
+		assertThat(saveButton,notNullValue());
+		LOGGER.debug("Saving the new Task");
+		
+		
 	}
 
 }
