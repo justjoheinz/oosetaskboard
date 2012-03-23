8b47fe45ec0f99cba75982b1bd49353fc4e23417
ma la <evilm@gmx.de>
2012-03-23 11:24:19 +0100

    testcommit

--- src/de/oose/taskboard/client/view/HasErrors.java
+++ src/de/oose/taskboard/client/view/HasErrors.java
@@ -3,6 +3,7 @@ package de.oose.taskboard.client.view;
 import de.oose.taskboard.shared.validation.Validatable;
 import de.oose.taskboard.shared.validation.ValidationResult;
 
+//interface
 public interface HasErrors {
 	
 	void displayErrors (ValidationResult<? extends Validatable> result);
