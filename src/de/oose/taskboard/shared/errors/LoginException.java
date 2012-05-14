5cdf1d996567689cacf5b0dcd49537ee1ddc1ad8
Markus Klink <justjoheinz@gmail.com>
2012-05-14 14:01:12 +0200

    added Exception/removed GIN

---
+++ src/de/oose/taskboard/shared/errors/LoginException.java
@@ -0,0 +1,25 @@
+package de.oose.taskboard.shared.errors;
+
+public class LoginException extends RuntimeException {
+
+	public LoginException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public LoginException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LoginException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LoginException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
