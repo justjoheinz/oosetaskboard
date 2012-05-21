55cb7d68418b5bb2aedf8fe1d50f0eb03d39a080
Markus Klink <justjoheinz@gmail.com>
2012-05-21 12:46:43 +0200

    Cookies for login

--- src/de/oose/taskboard/client/presenter/LoginPresenter.java
+++ src/de/oose/taskboard/client/presenter/LoginPresenter.java
@@ -1,20 +1,28 @@
 package de.oose.taskboard.client.presenter;
 
+
+import java.util.Date;
+
 import javax.inject.Inject;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.shared.HandlerManager;
+import com.google.gwt.user.client.Cookies;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.HasWidgets;
 
 import de.oose.taskboard.client.event.LoginEvent;
 import de.oose.taskboard.client.service.LoginServiceAsync;
-import de.oose.taskboard.client.util.DefaultAsyncCallback;
 import de.oose.taskboard.client.view.LoginView;
 import de.oose.taskboard.shared.bo.UserBO;
+import de.oose.taskboard.shared.errors.LoginException;
+import de.oose.taskboard.shared.validation.ValidationError;
+import de.oose.taskboard.shared.validation.ValidationResult;
 
 public class LoginPresenter implements Presenter {
 	
+	private static final String COOKIE = "taskboardUser";
 	private LoginView display;
 	private HandlerManager eventBus;
 	private LoginServiceAsync loginService;
@@ -31,8 +39,16 @@ public class LoginPresenter implements Presenter {
 		this.eventBus = eventBus;
 		this.loginService = loginService;
 		bind();
+		initCookieInformation();
 	}
 	
+	private void initCookieInformation() {
+		String userName = Cookies.getCookie(COOKIE);
+		if (userName != null) {
+			display.getTextBox().setText(userName);
+		}
+	}
+
 	private void bind() {
 		display.getBtnOK().addClickHandler(new ClickHandler() {
 			
@@ -40,10 +56,24 @@ public class LoginPresenter implements Presenter {
 			public void onClick(ClickEvent event) {
 				String account = display.getTextBox().getText();
 				boolean create = display.getBoxCreate().getValue();
-			    loginService.getAccount(account, create, new DefaultAsyncCallback<UserBO>() {
+			    loginService.getAccount(account, create, new AsyncCallback<UserBO>() {
+			    	
+			    	@Override
+			    	public void onFailure(Throwable caught) {
+			    		if (caught instanceof LoginException) {
+			    			LoginException ex = (LoginException) caught;
+			    			ValidationResult<String> result = new ValidationResult<String>();
+			    			result.add(new ValidationError<String>(null, null, ex.getMessage()));
+			    			display.displayErrors(result);
+			    		}
+			    	}
 
 					@Override
 					public void onSuccess(UserBO result) {
+						display.hide();
+						Date now = new Date();
+						long nowLong = now.getTime() + (1000 * 60 * 60 * 24 * 14); // 14 days later
+						Cookies.setCookie(COOKIE, result.getName(),new Date(nowLong));
 						eventBus.fireEvent(new LoginEvent(result));
 					}
 				});
