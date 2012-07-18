df2f1464af3298ef17c5dab91975f01914ad09ba
Markus Klink <justjoheinz@gmail.com>
2012-07-18 15:11:38 +0200

    added javadoc

--- src/de/oose/taskboard/client/util/DefaultAsyncCallback.java
+++ src/de/oose/taskboard/client/util/DefaultAsyncCallback.java
@@ -3,6 +3,12 @@ package de.oose.taskboard.client.util;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
+/** A small utility class which displays the exceptions message on failure to a Window.
+ * 
+ * @author markusklink
+ *
+ * @param <T> the result type of the asynchronous callback
+ */
 public abstract class DefaultAsyncCallback<T> implements AsyncCallback<T>{
 
 	@Override
